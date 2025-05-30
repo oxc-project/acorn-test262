e7b535fafb91d716feed6fbd8ddb1ff2dddaf3e4
overlookmotel <theoverlookmotel@gmail.com>
2025-05-30 11:43:38 +0100

    test Acorn and Meriyah ASTs match

--- src/test262.js
+++ src/test262.js
@@ -1,8 +1,19 @@
 import { Parser as AcornParser } from 'acorn';
+import { expect } from 'expect';
 import { parse as meriyahParse } from 'meriyah';
+import fs from 'node:fs/promises';
+import { dirname, join as pathJoin } from 'node:path';
 import YAML from 'yaml';
-import { transformerAcorn } from './utils/json.js';
+import { stringifyWith, transformerAcorn } from './utils/json.js';
 import { run } from './utils/run.js';
+
+const TICKS = '```';
+const MERIYAH_MISMATCHES_DIR_PATH = pathJoin(import.meta.dirname, '../meriyah_mismatches');
+
+await fs.mkdir(MERIYAH_MISMATCHES_DIR_PATH, { recursive: true });
+await fs.rm(pathJoin(MERIYAH_MISMATCHES_DIR_PATH, 'test'), { recursive: true, force: true });
+
+let meriyahParseFails = 0, meriyahMismatches = 0;
 
 await run({
   submodule: 'test262',
@@ -35,30 +46,47 @@ await run({
       ast = AcornParser.parse(code, {
         ecmaVersion: 'latest',
         sourceType: isModule ? 'module' : 'script',
-        preserveParens: true,
+        preserveParens: false, // TODO: Should be `true`
         allowHashBang: true,
         allowReturnOutsideFunction: true,
         // Note: Do not specify `allowAwaitOutsideFunction` option.
         // It defaults to `true` for modules, `false` for scripts, which is what we want.
       });
     } catch (acornErr) {
-      try {
-        ast = meriyahParse(code, {
-          module: isModule,
-          impliedStrict: isModule,
-          ranges: true,
-          raw: true,
-          preserveParens: true,
-          globalReturn: true,
-          webcompat: true, // I think this enables support for Annex B
-          next: true, // Enable parsing decorators and import attributes
-        });
-      } catch (meriyahErr) {
-        // console.log(acornErr.message);
-        return;
-      }
+      // console.log(acornErr.message);
+      return;
+    }
 
-      fixMeriyahValue(ast, code);
+    try {
+      const meriyahAst = meriyahParse(code, {
+        module: isModule,
+        impliedStrict: isModule,
+        ranges: true,
+        raw: true,
+        preserveParens: false, // TODO: Should be `true`
+        globalReturn: true,
+        webcompat: true, // I think this enables support for Annex B
+        next: true, // Enable parsing decorators and import attributes
+      });
+
+      fixMeriyahValue(meriyahAst, code);
+
+      const acornJson = stringifyWith(ast, transformerAcorn);
+      const meriyahJson = stringifyWith(meriyahAst, transformerAcorn);
+
+      if (meriyahJson !== acornJson) {
+        let diff = '';
+        try {
+          expect(meriyahJson).toEqual(acornJson);
+        } catch (err) {
+          diff = err.message.replace(/\[\d+m/g, '');
+        }
+        await writeMeriyahFailReport(path, code, `\n${TICKS}diff\n${diff}\n${TICKS}\n`);
+        meriyahMismatches++;
+      }
+    } catch (err) {
+      await writeMeriyahFailReport(path, code, `\n${TICKS}error\n${err?.message}\n${err?.stack}\n${TICKS}\n`);
+      meriyahParseFails++;
     }
 
     return [
@@ -69,6 +97,9 @@ await run({
     ];
   },
 });
+
+console.log('Meriyah parse failures:', meriyahParseFails);
+console.log('Meriyah mismatches:', meriyahMismatches);
 
 // Alter Meriyah's AST to fix mistakes and to match Acorn's
 function fixMeriyahValue(value, code) {
@@ -188,4 +219,11 @@ function fixMeriyahNode(node, code) {
       node.start = node.decorators[0].start;
     }
   }
+}
+
+async function writeMeriyahFailReport(path, code, report) {
+  report = `submodules/test262/${path}\n\n${TICKS}js\n${code}${TICKS}\n${report}`;
+  const reportPath = pathJoin(MERIYAH_MISMATCHES_DIR_PATH, `${path}.md`);
+  await fs.mkdir(dirname(reportPath), { recursive: true });
+  await fs.writeFile(reportPath, report);
 }
