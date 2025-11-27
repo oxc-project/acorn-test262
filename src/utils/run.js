51a6673f88db5699e9f57429b7b44aade36c3679
overlookmotel <theoverlookmotel@gmail.com>
2025-11-27 10:40:39 +0000

    refactor: reduce FS writes

--- src/utils/run.js
+++ src/utils/run.js
@@ -16,23 +16,11 @@ export async function run({ submodule, subDirectory, filter, transform, process 
   const outputsDirPath = pathJoin(OUTPUTS_DIR_PATH, submodule);
 
   // Find all files in fixtures dir
-  const files = await fs.readdir(pathJoin(fixturesRootPath, subDirectory), {
-    recursive: true,
-    withFileTypes: true,
-  });
+  const fixturePaths = await getFixturePaths(fixturesRootPath, subDirectory, filter);
 
-  const trimLen = fixturesRootPath.length + 1;
-  const fixturePaths = [];
-  for (const file of files) {
-    if (file.isFile()) {
-      const fixturePath = pathJoin(file.parentPath.slice(trimLen), file.name);
-      if (!filter || filter(fixturePath)) fixturePaths.push(fixturePath);
-    }
-  }
-
-  // Empty outputs directory
-  await fs.rm(outputsDirPath, { recursive: true, force: true });
-  await fs.mkdir(outputsDirPath, { recursive: true });
+  // Find all existing files in outputs dir.
+  // We use this to identify outdated output files and delete them at the end.
+  const oldOutputPaths = new Set(await getOutputPaths(outputsDirPath));
 
   // Run all fixtures
   let parseFailCount = 0;
@@ -46,6 +34,7 @@ export async function run({ submodule, subDirectory, filter, transform, process 
       continue;
     }
 
+    // Write outputs
     for (const output of outputs) {
       const outputPath = pathJoin(outputsDirPath, output.path);
       await fs.mkdir(dirname(outputPath), { recursive: true });
@@ -53,8 +42,91 @@ export async function run({ submodule, subDirectory, filter, transform, process 
       let { content } = output;
       if (content == null) content = stringifyWith(output.ast, transform);
 
-      await fs.writeFile(outputPath, content);
+      // Write output if it's different from existing file.
+      // We avoid re-writing outputs which are unchanged in order to minimize touching files.
+      // If we rewrite all files, it makes Git really slow, as it has to re-index.
+      let oldContent = null;
+      try {
+        oldContent = await fs.readFile(outputPath, "utf8");
+      } catch (err) {
+        if (err?.code !== "ENOENT") throw err;
+      }
+      if (content !== oldContent) await fs.writeFile(outputPath, content);
+
+      // Delete from `oldOutputPaths` so it's not deleted at the end
+      oldOutputPaths.delete(output.path);
     }
   }
+
+  // Delete old outputs
+  for (let path of oldOutputPaths) {
+    path = pathJoin(outputsDirPath, path);
+    await fs.rm(path);
+
+    // If directory is now empty, delete it
+    while (true) {
+      path = dirname(path);
+      try {
+        await fs.rmdir(path);
+      } catch (err) {
+        if (err?.code !== "ENOTEMPTY") throw err;
+        break;
+      }
+    }
+  }
+
   console.log(`\nFailed to parse: ${parseFailCount}`);
+}
+
+/**
+ * Get paths of fixtures in a submodule to process.
+ *
+ * Returns an array of paths relative to the submodule directory (`fixturesRootPath`).
+ *
+ * @param {string} fixturesRootPath - Full path to root of the submodule
+ * @param {string} subDirectory - Relative path to the directory in submodule containing fixtures
+ * @param {function} [filter] - Function to filter fixtures (optional).
+ *   Is passed the path of the fixture relative to fixtures subdirectory.
+ *   Should return `true` if the fixture should be processed, or `false` otherwise.
+ * @returns {Array<string>} List of fixture paths
+ */
+async function getFixturePaths(fixturesRootPath, subDirectory, filter) {
+  const files = await fs.readdir(pathJoin(fixturesRootPath, subDirectory), {
+    recursive: true,
+    withFileTypes: true,
+  });
+
+  const fixturePaths = [],
+    trimLen = fixturesRootPath.length + 1;
+  for (const file of files) {
+    if (!file.isFile()) continue;
+
+    const path = pathJoin(file.parentPath.slice(trimLen), file.name);
+    if (!filter || filter(path)) fixturePaths.push(path);
+  }
+
+  return fixturePaths;
+}
+
+/**
+ * Get paths of output files in the output directory for a submodule.
+ *
+ * Returns an array of paths relative to the output directory (`outputsDirPath`).
+ *
+ * @param {string} outputsDirPath - Full path to the root of output directory for submodule
+ * @returns {Array<string>} List of output paths relative to the output directory
+ */
+async function getOutputPaths(outputsDirPath) {
+  const files = await fs.readdir(outputsDirPath, { recursive: true, withFileTypes: true });
+
+  const outputPaths = [],
+    trimLen = outputsDirPath.length + 1;
+  for (const file of files) {
+    if (!file.isFile()) continue;
+
+    const path = pathJoin(file.parentPath.slice(trimLen), file.name);
+    outputPaths.push(path);
+  }
+
+  return outputPaths;
 }
